0d1ec6eb9d71e871e556dd4a589cde4da86aca4b
hedengyao <dengyao.he@renee-arts.com>
2019-12-13 14:48:48 +0800

    11

---
+++ js/other.js
@@ -0,0 +1,10 @@
+var urlheader = './html/common/header.html';
+var urlfooter = './html/common/footer.html';
+
+
+$('header').load(urlheader, function(){
+    $('#header_top').click(function(){
+        $('#bs_navbar').slideToggle();
+    });
+});
+$('footer').load(urlfooter);
