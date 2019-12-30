883094509c94ecea50d3817f6984aaf4f2ba7a16
jianfeng.xue <jianfeng_8331@163.com>
2019-12-30 13:46:28 +0800

    实现pc端打电话小图标的问题

--- js/other.js
+++ js/other.js
@@ -30,3 +30,12 @@ $(".wechat").hover(function(){
 },function(){
     $(".wechat_img").css("display","none");
 });
+
+var content = document.getElementById('body');
+content.style = "position: relative;";
+var customerService = document.createElement("div");
+customerService.className = "lisAndPhone"
+customerService.style = "position: absolute; top:640px; right:24px;"
+customerService.innerHTML = `<img style="width: 48px; height: 48px; background-color: #0D4C9A; display: block;border-bottom: 1px solid #fff;cursor: pointer;" src="../imgs/在线联系.png">
+                            <img style="width: 48px; height: 48px; background-color: #0D4C9A;cursor: pointer;" src="../imgs/电话联系.png">`
+content.appendChild(customerService);
