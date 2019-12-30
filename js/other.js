7548ceccce91fe882cc1a815e2162a1c025296db
贺登耀 <hedengyao@lexianyi.com>
2019-12-30 15:39:11 +0800

    解决冲突

--- js/other.js
+++ js/other.js
@@ -12,60 +12,49 @@ $('footer').load(urlfooter);
 
 
 // header 切换效果展示
+var Location = window.location.href;
+var currentLocation = Location.split('/').pop();
 
-    var Location = window.location.href;
-    var currentLocation = Location.split('/').pop();
+console.log(currentLocation)
+setTimeout(() => {
     switch (currentLocation) {
         case "index.html":
-            console.log(document.getElementsByClassName('index')[0])
-            document.getElementsByClassName('index')[0].style.color="rgba(13, 76, 154, 1)"
-            // $('.index').css('color', ' rgba(13, 76, 154, 1)');
+            $('.index_1').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
 
-            //$('.index').css('border-bottom', '2px solid #e74b4b')
             break;
         case "production.html":
-            $('.product').css('color', ' rgba(13, 76, 154, 1)');
-            // $('.product').css('border-bottom', '2px solid #e74b4b')
+            $('.production_2').css('color', ' rgba(13, 76, 154, 1)');
+            $('.production_2').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
+
             break;
         case "solution.html":
-            $('.solution').css('color', ' rgba(13, 76, 154, 1)');
-            //$('.download').css('border-bottom', '2px solid #e74b4b')
+            $('.solution_3').css('color', ' rgba(13, 76, 154, 1)');
+            $('.solution_3').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
+
             break;
         case "cooperative.html":
-            $('.cooperative').css('color', ' rgba(13, 76, 154, 1)');
-            //$('.service').css('border-bottom', '2px solid #e74b4b')
+            $('.cooperative_4').css('color', ' rgba(13, 76, 154, 1)');
+            $('.cooperative_4').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
+
             break;
         case "new_center.html":
-            $('.new_center').css('color', ' rgba(13, 76, 154, 1)');
-            //$('.help_center').css('border-bottom', '2px solid #e74b4b')
+            $('.new_center_5').css('color', ' rgba(13, 76, 154, 1)');
+            $('.new_center_5').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
+
             break;
         case "about_us.html":
-            $('.about_us').css('color', ' rgba(13, 76, 154, 1)');
-            //$('.help_center').css('border-bottom', '2px solid #e74b4b')
+            $('.about_us_6').css('color', ' rgba(13, 76, 154, 1)');
+            $('.about_us_6').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
+
             break;
         case "contact_us.html":
-            $('.contact_us').css('color', ' rgba(13, 76, 154, 1)');
-            //$('.help_center').css('border-bottom', '2px solid #e74b4b')
+            $('.contact_us_7').css('color', ' rgba(13, 76, 154, 1)');
+            $('.contact_us_7').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
+
             break;
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}, 100)
 
 
 
@@ -86,23 +75,18 @@ $(".weibo").hover(function () {
 
 $(".wechat").hover(function () {
     console.log("2222")
-<<<<<<< HEAD
     $(".wechat_img").css("display", "block");
 }, function () {
     $(".wechat_img").css("display", "none");
 });
-=======
-    $(".wechat_img").css("display","block");
-},function(){
-    $(".wechat_img").css("display","none");
-});
 
+
+// fixed  服务 联系方式
 var content = document.getElementById('body');
 content.style = "position: relative;";
 var customerService = document.createElement("div");
 customerService.className = "lisAndPhone"
-customerService.style = "position: absolute; top:640px; right:24px;"
+customerService.style = "position: fixed; top:640px; right:24px;z-index:100"
 customerService.innerHTML = `<img style="width: 48px; height: 48px; background-color: #0D4C9A; display: block;border-bottom: 1px solid #fff;cursor: pointer;" src="../imgs/在线联系.png">
                             <img style="width: 48px; height: 48px; background-color: #0D4C9A;cursor: pointer;" src="../imgs/电话联系.png">`
 content.appendChild(customerService);
->>>>>>> 6352b77399f0be5d59e416072f1da7d2547d597b
