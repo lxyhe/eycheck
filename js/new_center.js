890e1b4877d74751d1a4604038d9f1c7a2133ff4
贺登耀 <hedengyao@lexianyi.com>
2019-12-30 14:33:06 +0800

    修改下拉框bug

--- js/new_center.js
+++ js/new_center.js
@@ -20,11 +20,11 @@ $('.list').click(()=>{
     window.location.href="./new_page.html"
 })
 kkpager.generPageHtml({
-    pno: 10,
+    pno: 1,
     //总页码
-    total: 100,
+    total: 10,
     //总数据条数
-    totalRecords: 1000,
+    totalRecords: 100,
     //链接前部
     hrefFormer: 'pager_test',
     //链接尾部
--- js/other.js
+++ js/other.js
@@ -2,8 +2,8 @@ var urlheader = './html/common/header.html';
 var urlfooter = './html/common/footer.html';
 
 
-$('header').load(urlheader, function(){
-    $('#header_top').click(function(){
+$('header').load(urlheader, function () {
+    $('#header_top').click(function () {
         $('#bs_navbar').slideToggle();
     });
 });
@@ -11,22 +11,82 @@ $('header').load(urlheader, function(){
 $('footer').load(urlfooter);
 
 
+// header 切换效果展示
 
+    var Location = window.location.href;
+    var currentLocation = Location.split('/').pop();
+    switch (currentLocation) {
+        case "index.html":
+            console.log(document.getElementsByClassName('index')[0])
+            document.getElementsByClassName('index')[0].style.color="rgba(13, 76, 154, 1)"
+            // $('.index').css('color', ' rgba(13, 76, 154, 1)');
+
+            //$('.index').css('border-bottom', '2px solid #e74b4b')
+            break;
+        case "production.html":
+            $('.product').css('color', ' rgba(13, 76, 154, 1)');
+            // $('.product').css('border-bottom', '2px solid #e74b4b')
+            break;
+        case "solution.html":
+            $('.solution').css('color', ' rgba(13, 76, 154, 1)');
+            //$('.download').css('border-bottom', '2px solid #e74b4b')
+            break;
+        case "cooperative.html":
+            $('.cooperative').css('color', ' rgba(13, 76, 154, 1)');
+            //$('.service').css('border-bottom', '2px solid #e74b4b')
+            break;
+        case "new_center.html":
+            $('.new_center').css('color', ' rgba(13, 76, 154, 1)');
+            //$('.help_center').css('border-bottom', '2px solid #e74b4b')
+            break;
+        case "about_us.html":
+            $('.about_us').css('color', ' rgba(13, 76, 154, 1)');
+            //$('.help_center').css('border-bottom', '2px solid #e74b4b')
+            break;
+        case "contact_us.html":
+            $('.contact_us').css('color', ' rgba(13, 76, 154, 1)');
+            //$('.help_center').css('border-bottom', '2px solid #e74b4b')
+            break;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 微博 公众号hover展示
 $(document).on("hover", ".weibo", function () {
     console.log("1111")
 });
 
-$(".weibo").hover(function(){
-    $(".weibo_img").css("display","block");
+$(".weibo").hover(function () {
+    $(".weibo_img").css("display", "block");
     console.log("1111")
-},function(){
-    $(".weibo_img").css("display","none");
+}, function () {
+    $(".weibo_img").css("display", "none");
 });
 
 
-$(".wechat").hover(function(){
+$(".wechat").hover(function () {
     console.log("2222")
-    $(".wechat_img").css("display","block");
-},function(){
-    $(".wechat_img").css("display","none");
+    $(".wechat_img").css("display", "block");
+}, function () {
+    $(".wechat_img").css("display", "none");
 });
