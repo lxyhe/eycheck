4cb3330d368b5cc251c61b4cd2464fb75cd11191
贺登耀 <hedengyao@lexianyi.com>
2020-01-02 10:35:29 +0800

    移动端下拉框

--- js/other.js
+++ js/other.js
@@ -16,69 +16,76 @@ var Location = window.location.href;
 var currentLocation = Location.split('/').pop();
 
 console.log(currentLocation)
+
+if (!(navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+
+
+    console.log("移动端")
+    setTimeout(() => {
+        switch (currentLocation) {
+            case "index.html":
+                $('.index_1').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
+
+                break;
+            case "production.html":
+                $('.production_2').css('color', ' rgba(13, 76, 154, 1)');
+                $('.production_2').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
+
+                break;
+            case "solution.html":
+                $('.solution_3').css('color', ' rgba(13, 76, 154, 1)');
+                $('.solution_3').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
+
+                break;
+            case "cooperative.html":
+                $('.cooperative_4').css('color', ' rgba(13, 76, 154, 1)');
+                $('.cooperative_4').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
+
+                break;
+            case "new_center.html":
+                $('.new_center_5').css('color', ' rgba(13, 76, 154, 1)');
+                $('.new_center_5').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
+
+                break;
+            case "about_us.html":
+                $('.about_us_6').css('color', ' rgba(13, 76, 154, 1)');
+                $('.about_us_6').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
+
+                break;
+            case "contact_us.html":
+                $('.contact_us_7').css('color', ' rgba(13, 76, 154, 1)');
+                $('.contact_us_7').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
+
+                break;
+        }
+
+    }, 100)
+}
+
+
+
+
+
+
+//微博 公众号hover展示
 setTimeout(() => {
-    switch (currentLocation) {
-        case "index.html":
-            $('.index_1').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
-
-            break;
-        case "production.html":
-            $('.production_2').css('color', ' rgba(13, 76, 154, 1)');
-            $('.production_2').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
-
-            break;
-        case "solution.html":
-            $('.solution_3').css('color', ' rgba(13, 76, 154, 1)');
-            $('.solution_3').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
-
-            break;
-        case "cooperative.html":
-            $('.cooperative_4').css('color', ' rgba(13, 76, 154, 1)');
-            $('.cooperative_4').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
-
-            break;
-        case "new_center.html":
-            $('.new_center_5').css('color', ' rgba(13, 76, 154, 1)');
-            $('.new_center_5').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
-
-            break;
-        case "about_us.html":
-            $('.about_us_6').css('color', ' rgba(13, 76, 154, 1)');
-            $('.about_us_6').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
-
-            break;
-        case "contact_us.html":
-            $('.contact_us_7').css('color', ' rgba(13, 76, 154, 1)');
-            $('.contact_us_7').css('border-bottom', '2px solid rgba(13, 76, 154, 1)');
-
-            break;
-    }
-
+    $(".wechat").hover(function () {
+        console.log("2222")
+        $(".wechat_img").css("display", "block");
+    }, function () {
+        $(".wechat_img").css("display", "none");
+    });
+    $(".weibo").hover(function () {
+        $(".weibo_img").css("display", "block");
+        console.log("1111")
+    }, function () {
+        $(".weibo_img").css("display", "none");
+    });
 }, 100)
 
 
 
 
-
-// 微博 公众号hover展示
-$(document).on("hover", ".weibo", function () {
-    console.log("1111")
-});
-
-$(".weibo").hover(function () {
-    $(".weibo_img").css("display", "block");
-    console.log("1111")
-}, function () {
-    $(".weibo_img").css("display", "none");
-});
-
-
-$(".wechat").hover(function () {
-    console.log("2222")
-    $(".wechat_img").css("display", "block");
-}, function () {
-    $(".wechat_img").css("display", "none");
-});
 
 
 // fixed  服务 联系方式
@@ -90,3 +97,64 @@ customerService.style = "position: fixed; top:640px; right:24px;z-index:100"
 customerService.innerHTML = `<img style="width: 48px; height: 48px; background-color: #0D4C9A; display: block;border-bottom: 1px solid #fff;cursor: pointer;" src="../imgs/在线联系.png">
                             <img style="width: 48px; height: 48px; background-color: #0D4C9A;cursor: pointer;" src="../imgs/电话联系.png">`
 content.appendChild(customerService);
+
+
+// 点击菜单栏展开页面
+setTimeout(() => {
+
+    $('.product_list1').css('display', 'none')
+    $('.solution_list1').css('display', 'none')
+    $('.arrow_1').click(() => {
+        if ($('.product_list1').css('display') == "block") {
+            $('.product_list1').css('display', 'none')
+            $('.arrow_1').css({
+                'transform': ' rotate(135deg)'
+            })
+        } else {
+            $('.product_list1').css('display', 'block')
+            $('.arrow_1').css({
+                'transform': ' rotate(-45deg)'
+            })
+        }
+
+    })
+
+    $('.arrow_2').click(() => {
+
+
+        if ($('.solution_list1').css('display') == "block") {
+            $('.solution_list1').css('display', 'none')
+            $('.arrow_2').css({
+                'transform': 'rotate(135deg)'
+            })
+        } else {
+            $('.solution_list1').css('display', 'block')
+            $('.arrow_2').css({
+                'transform': 'rotate(-45deg)'
+            })
+        }
+    })
+
+    $(".scheme_1").on("click", function () {
+        location.href = "./personal_plain_info.html";
+    })
+    $(".scheme_2").on("click", function () {
+        location.href = "./company_plain_info.html"
+    })
+    $(".scheme_3").on("click", function () {
+        location.href = "./gover_coop.html"
+    })
+    // 
+    $(".scheme_4").on("click", function () {
+        location.href = "./solution_net.html";
+    })
+    $(".scheme_5").on("click", function () {
+        location.href = "./solition_cross.html"
+    })
+    $(".scheme_6").on("click", function () {
+        location.href = "./solution_wisdom.html"
+    })
+    $(".scheme_7").on("click", function () {
+        location.href = "./solution_hotel.html"
+    })
+}, 100)
